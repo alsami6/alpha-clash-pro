ff1502646da3a995663ec51042bb3f1b78d7ad8c
Al <alsami1209@gmail.com>
2024-03-22 15:59:21 +0600

    allmost done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -6,6 +6,35 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+
+function handleKeyboardKeyUpEvent(){
+    const playerPressed = event.key;
+    console.log('Player Prssed', playerPressed);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlhabet = currentAlphabet.toLocaleLowerCase();
+    console.log(playerPressed, expectedAlhabet);
+
+    if(playerPressed === expectedAlhabet){
+        console.log("You get a point");
+    }
+    else{
+        console.log("Oppps you missed try again");
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
+function continueGame(){
+    const alphabet = GetARandomAlphabet();
+    console.log('Your random alphabet is', alphabet);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+}
+
 function play(){
-    hideElementById('home-screen')
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,4 +1,20 @@
-function hideElementById(elementById){
-    const element = document.getElementById('elementId');
+function hideElementById(elementId){
+    const element = document.getElementById('home-screen');
     element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById('play-ground');
+    element.classList.remove('hidden');
+}
+
+function GetARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
 }
